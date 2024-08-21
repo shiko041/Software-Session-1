3472eab9bfb4ddee324b52ff1d77ba9831091131
Ahmad Hossam <149507985+Ahmad-Hossam-88@users.noreply.github.com>
2024-08-21 22:59:08 +0300

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,6 +1,7 @@
+int number = 4;
+int boxes[number];
 for (int i = 0; i < number - 1; i++)
         for (int j = 0; j < number - 1 - i; j++)
-
             if (boxes[j] > boxes[j + 1])
             {
                 int hold = boxes[j];
