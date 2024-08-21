4ce1cb23e7f37da8894069d64887caff35624658
AHmad-Hossam-88 <ahmad.hossam.8818@gmail.com>
2024-08-21 22:56:21 +0300

    cpp file

---
+++ BubbleSort.cpp
@@ -0,0 +1,9 @@
+for (int i = 0; i < number - 1; i++)
+        for (int j = 0; j < number - 1 - i; j++)
+
+            if (boxes[j] > boxes[j + 1])
+            {
+                int hold = boxes[j];
+                boxes[j] = boxes[j + 1];
+                boxes[j + 1] = hold;
+            }
